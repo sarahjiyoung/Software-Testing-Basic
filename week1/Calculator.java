595a52e1fae76255a2d4aabc077f045b96896f1d
sarahjiyoung <sarahhong5049@gmail.com>
2025-09-15 20:19:11 +0900

    Week1: Handle division by zero in division and modulo

--- week1/Calculator.java
+++ week1/Calculator.java
@@ -12,8 +12,21 @@ public class Calculator {
         int b = sc.nextInt();
 
         System.out.println("a = " + a + ", b = " + b);
+
+        // 1차 commit: 더하기, 빼기
         System.out.println("a + b = " + (a + b));
         System.out.println("a - b = " + (a - b));
+
+        // 2차 commit: 곱하기, 나누기, 나머지
+        System.out.println("a * b = " + (a * b));
+
+        if (b != 0) {
+            System.out.println("a / b = " + (a / b));   // 정수 나눗셈 (몫)
+            System.out.println("a % b = " + (a % b));   // 나머지
+        } else {
+            System.out.println("a / b = 0으로 나눌 수 없습니다.");
+            System.out.println("a % b = 0으로 나눌 수 없습니다.");
+        }
 
         sc.close();
     }
